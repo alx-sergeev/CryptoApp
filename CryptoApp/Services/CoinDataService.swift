f99fe97d7dde4325b220a18fc88c9e417a021aa3
Alexander Sergeev <sergeev_ul73@mail.ru>
2023-10-04 13:07:54 +0400

    Добавил новый сетевой слой, класс NetworkManager
    Класс для работы с сетевыми запросами

--- CryptoApp/Services/CoinDataService.swift
+++ CryptoApp/Services/CoinDataService.swift
@@ -24,27 +24,11 @@ class CoinDataService {
             let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=rub&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return
         }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkManager.download(url: url)
             .decode(type: [Coin].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("Error: \(error.localizedDescription)")
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
