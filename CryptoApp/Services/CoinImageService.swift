7277e46bd92e663ce22ce3ed81afd7bc50fbe1ee
Alexander Sergeev <sergeev_ul73@mail.ru>
2023-10-07 12:04:05 +0400

    Новый класс LocaleFileManager, fix отображения иконок
    * Класс для работы с изображениями в файловом менеджере;
    * Применил новый класс для отображения иконок валют, если иконка не сохранена на устройстве, скачиваем из сети, иначе берем из файлового менеджера.

--- CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/Services/CoinImageService.swift
@@ -12,16 +12,30 @@ import Combine
 class CoinImageService {
     private let coin: Coin
     private var imageSubscription: AnyCancellable?
+    private let fileManager = LocaleFileManager.shared
+    private let folderName = "coin_images"
+    private let imageName: String
     
     @Published var image: UIImage?
     
     
     init(coin: Coin) {
         self.coin = coin
+        
+        imageName = coin.id
         getCoinImage()
     }
     
+    
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkManager.download(url: url)
@@ -29,8 +43,11 @@ class CoinImageService {
                 return UIImage(data: data)
             }
             .sink(receiveCompletion: NetworkManager.handleCompletion) { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             }
     }
 }
